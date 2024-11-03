c737796976bfef80aba1b3cf216180c1e70dc354
DaniaAlvarado <dania26alvarado@gmail.com>
2024-11-03 12:42:51 -0500

    fin seccion 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'bases';
+  public title: string = 'bases'; // public y string son opcionales, 'bases' puede ser cualquier nombre
+
 }
